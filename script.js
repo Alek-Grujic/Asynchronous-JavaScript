e3c1801cef198482f6bb6a6dcc3cbdc32372ef67
Alek-Grujic <alek.grujic001@outlook.com>
2026-02-12 10:28:06 +0100

    feat: fetch and render neighbouring country using chained promises

--- script.js
+++ script.js
@@ -66,7 +66,14 @@ const renderCountry = function (data, className = "") {
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then((response) => response.json())
-    .then((data) => renderCountry(data[0]));
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then((response) => response.json())
+    .then((data) => renderCountry(data, "neighbour"));
 };
 
 getCountryData("portugal");
