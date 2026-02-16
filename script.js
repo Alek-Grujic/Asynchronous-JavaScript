96ec9b86ec1dd6fe2da4a5df1df09de82f0ec486
Alek-Grujic <alek.grujic001@outlook.com>
2026-02-16 09:47:06 +0100

    feat: finish whereAmI challenge with full promise chaining and error handling

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ function renderError(msg) {
 }
 
 const renderCountry = function (data, className = "") {
+  console.log(data);
   const html = `
             <article class="country ${className}">
             <img class="country__img" src="${data.flag}" />
@@ -102,8 +103,69 @@ const getJSON = function (url, errMsg = "Something went wrong") {
   });
 };
 
+// const getCountryData = function (country) {
+//   getJSON(`https://restcountries.com/v2/name/${country}`, `Country not found`)
+//     .then((data) => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
+//       console.log(neighbour);
+
+//       if (!neighbour) throw new Error(`No neighbour found!`);
+
+//       return getJSON(
+//         `https://restcountries.com/v2/alpha/${neighbour}`,
+//         `Country not found`,
+//       );
+//     })
+//     .then((data) => renderCountry(data, "neighbour"))
+//     .catch((err) => {
+//       console.error(`${err}`);
+//       renderError(`Something went wrong - ${err} - Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// getCountryData("portugal");
+
+// btn.addEventListener("click", function () {
+//   getCountryData("australia");
+// });
+
+/////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.
+The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=52.508&longitude=13.381. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
 const getCountryData = function (country) {
-  getJSON(`https://restcountries.com/v2/name/${country}`, `Country not found`)
+  return getJSON(
+    `https://restcountries.com/v2/name/${country}`,
+    `Country not found`,
+  )
     .then((data) => {
       renderCountry(data[0]);
       const neighbour = data[0].borders?.[0];
@@ -116,7 +178,32 @@ const getCountryData = function (country) {
         `Country not found`,
       );
     })
-    .then((data) => renderCountry(data, "neighbour"))
+    .then((data) => renderCountry(data, "neighbour"));
+  // .catch((err) => {
+  //   console.error(`${err}`);
+  //   renderError(`Something went wrong - ${err} - Try again!`);
+  // })
+  // .finally(() => {
+  //   countriesContainer.style.opacity = 1;
+  // });
+};
+
+function whereAmI(lat, lng) {
+  return fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Something went wrong - ${response.status}`);
+      }
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.countryName}`);
+      return getCountryData(data.countryName);
+    })
     .catch((err) => {
       console.error(`${err}`);
       renderError(`Something went wrong - ${err} - Try again!`);
@@ -124,10 +211,8 @@ const getCountryData = function (country) {
     .finally(() => {
       countriesContainer.style.opacity = 1;
     });
-};
-
-// getCountryData("portugal");
+}
 
 btn.addEventListener("click", function () {
-  getCountryData("australia");
+  whereAmI(19.037, 72.873);
 });
