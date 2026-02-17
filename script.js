618af491a354d234fee80b167bbcd9f9bd04ede8
Alek-Grujic <alek.grujic001@outlook.com>
2026-02-17 14:53:30 +0100

    chore: demonstrate promise microtask priority over setTimeout macrotask

--- script.js
+++ script.js
@@ -304,3 +304,20 @@ class FindLocation {
 
 const firstTry = new FindLocation(45.037, 72.873);
 firstTry.init();
+
+///////////////////////////////////////////////////////////////////////
+
+console.log("Test start");
+
+setTimeout(() => console.log("0 sec timer"), 0);
+
+Promise.resolve("Resolved promise 1").then((res) => console.log(res));
+
+Promise.resolve("Resolved promise 2").then((res) => {
+  for (let i = 0; i < 10000; i++) {}
+  console.log(res);
+});
+
+console.log("Test end");
+
+//////////////////////////////////////////////////////////////////////
