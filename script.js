10708eef692d98a8068eb5dfcb7661e2740cb552
Alek-Grujic <alek.grujic001@outlook.com>
2026-02-17 15:49:21 +0100

    feat: implement custom lotteryPromise to demonstrate resolve and reject flow

--- script.js
+++ script.js
@@ -321,3 +321,18 @@ Promise.resolve("Resolved promise 2").then((res) => {
 console.log("Test end");
 
 //////////////////////////////////////////////////////////////////////
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log("Lotter draw is happening");
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve("You Win!");
+    } else {
+      reject(new Error("You lost your money"));
+    }
+  }, 2000);
+});
+
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
