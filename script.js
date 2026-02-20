13d77ba68e10a62f2078a92aeff344a1c7c798b3
Alek-Grujic <alek.grujic001@outlook.com>
2026-02-20 12:04:33 +0100

    feat: implement Promise.race with timeout handling for fetch requests

--- script.js
+++ script.js
@@ -496,7 +496,7 @@ const whereAmI = async function () {
 //   whereAmI();
 // });
 
-console.log("1: Will get location");
+// console.log("1: Will get location");
 // const city = whereAmI();
 // console.log(city);
 
@@ -507,15 +507,15 @@ console.log("1: Will get location");
 //     console.log("3: Finished getting location");
 //   });
 
-(async function () {
-  try {
-    const cityAndCountry = await whereAmI();
-    console.log(`2: ${cityAndCountry}`);
-  } catch (err) {
-    console.error(`2: ${err.message}`);
-  }
-  console.log("3: Finished getting location");
-})();
+// (async function () {
+//   try {
+//     const cityAndCountry = await whereAmI();
+//     console.log(`2: ${cityAndCountry}`);
+//   } catch (err) {
+//     console.error(`2: ${err.message}`);
+//   }
+//   console.log("3: Finished getting location");
+// })();
 
 ///////////////////////////////////////
 
@@ -546,9 +546,32 @@ const get3Countries = async function (c1, c2, c3) {
       getJSON(`https://restcountries.com/v2/name/${c2}`),
       getJSON(`https://restcountries.com/v2/name/${c3}`),
     ]);
-    console.log(data.map((d) => d[0].capital));
+    // console.log(data.map((d) => d[0].capital));
   } catch (err) {
     console.error(err);
   }
 };
 get3Countries("portugal", "canada", "tanzania");
+
+/////////////////////////////////////////////////////////////////
+
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/egypt`),
+    getJSON(`https://restcountries.com/v2/name/mexico`),
+  ]);
+  //   console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error("Request took too long!"));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([getJSON(`https://restcountries.com/v2/name/italy`), timeout(0.1)])
+  .then((res) => console.log(res[0]))
+  .catch((err) => console.error(err));
